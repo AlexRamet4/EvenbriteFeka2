cec4c4e88d147001ac94cc2e12e9231c5ab176e4
Javier <javi_roig@hotmail.es>
2021-03-22 19:47:41 +0100

    first commit

---
+++ src/java/Registro_InicioSesion/ServletRegistarse.java
@@ -0,0 +1,80 @@
+package Registro_InicioSesion;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Entity.Rol;
+import Entity.Usuario;
+import dao.UsuarioFacade;
+import java.io.IOException;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Javier
+ */
+@WebServlet(urlPatterns = {"/ServletRegistrarse"})
+public class ServletRegistarse extends HttpServlet {
+
+    @EJB
+    private UsuarioFacade usuarioFacade;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+                
+        String nombre = new String(req.getParameter("nombre").getBytes("ISO-8859-1"), "UTF-8");
+        String primerApellido = new String(req.getParameter("primerApellido").getBytes("ISO-8859-1"), "UTF-8");
+        String segundoApellido = new String(req.getParameter("segundoApellido").getBytes("ISO-8859-1"), "UTF-8");
+        String domicilio = new String(req.getParameter("domicilio").getBytes("ISO-8859-1"), "UTF-8");
+        String edad = new String(req.getParameter("edad").getBytes("ISO-8859-1"), "UTF-8");
+        String ciudad = new String(req.getParameter("ciudad").getBytes("ISO-8859-1"), "UTF-8");
+        String sexo = new String(req.getParameter("sexo").getBytes("ISO-8859-1"), "UTF-8");
+        String correo = new String(req.getParameter("correo").getBytes("ISO-8859-1"), "UTF-8");
+        String password = new String(req.getParameter("password").getBytes("ISO-8859-1"), "UTF-8");
+        String confPassword = new String(req.getParameter("confPassword").getBytes("ISO-8859-1"), "UTF-8");
+        
+        String mensaje = "";
+        Usuario user = usuarioFacade.find(correo);
+        //Usuario usuario = usuarioFacade.findAll().stream().filter(x -> x.getEmail().equals(correo)).findAny().get();
+
+        if (!password.equals(confPassword)) {
+            mensaje = "Las contraseñas no son iguales";
+        } else if (user != null) {
+            mensaje = "Este correo ya está en uso";
+        } else {
+            Usuario nuevoUsuario = new Usuario(correo, nombre, password, primerApellido, segundoApellido, domicilio, ciudad, sexo, Integer.parseInt(edad));
+            nuevoUsuario.setIdRol(new Rol(3));
+            usuarioFacade.create(nuevoUsuario);
+            mensaje = "Has sido registrado con éxito!!!";
+        }
+        
+        HttpSession session = req.getSession();
+        session.setAttribute("mensajeRegistro", mensaje);
+        
+        RequestDispatcher rd;
+        rd = req.getRequestDispatcher("Registrarse.jsp");
+        rd.forward(req, res);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/Registrarse.jsp
@@ -0,0 +1,67 @@
+<%-- 
+    Document   : Registrarse
+    Created on : 20-mar-2021, 19:27:50
+    Author     : Javier
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html style="height: 100%">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link href="./styles/forms.css" rel="stylesheet">
+        <title> EvenbriteFeka </title>
+        <link rel="icon" type="image/png" href="https://cdn.evbstatic.com/s3-build/perm_001/8a4e23/django/images/favicons/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="https://cdn.evbstatic.com/s3-build/perm_001/477279/django/images/favicons/favicon-194x194.png" sizes="194x194">
+    </head>
+    
+    <%
+        boolean registroCompletado = false;
+        String mensaje = "";
+        mensaje = (String) session.getAttribute("mensajeRegistro");
+        if (mensaje == null) {
+            mensaje = "";
+        }
+        registroCompletado = !mensaje.equals("");    
+    %>
+    
+    <body style="height: 100%">
+        <center>
+            <h1 class="<%= registroCompletado ? "title" : "hidden title" %>"> <%= mensaje %> </h1>
+        </center>
+        
+        <form action="ServletRegistrarse" method="POST" class="d-flex flex-column flex-wrap align-items-center justify-content-center" style="height: 100%">
+            
+            <input type="text" name="nombre" class="form-control size" placeholder="Nombre">
+
+            <input type="text" name="primerApellido" class="form-control size" placeholder="Primer apellido">
+
+            <input type="text" name="segundoApellido" class="form-control size" placeholder="Segundo apellido">
+
+            <input type="text" name="domicilio" class="form-control size" placeholder="Domicilio">
+
+            <input type="text" name="ciudad" class="form-control size" placeholder="Ciudad de residencia">
+
+            <input type="number" name="edad" class="form-control size" placeholder="Edad">
+
+            <select name="sexo" class="form-select size" aria-label="Default select example">
+                <option selected>Elige tu sexo</option>
+                <option value="hombre">Hombre</option>
+                <option value="mujer">Mujer</option>
+                <option value="otro">Otro</option>
+            </select>
+            
+            <input type="email" name="correo" class="form-control size" placeholder="Correo">
+
+            <input type="password" name="password" class="form-control size" placeholder="Contraseña">
+            
+            <input type="password" name="confPassword" class="form-control size" placeholder="Confirmar contraseña">
+            
+            <input class="btn btn-primary" type="submit">
+            
+        </form>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    </body>
+</html>
