538aa93a45eb01f6f3770eb7bde1b4f9b82c54ad
Javier <javi_roig@hotmail.es>
2021-03-24 21:45:47 +0100

    Inicio de sesión

---
+++ src/java/Registro_InicioSesion/ServletIniciarSesion.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Registro_InicioSesion;
+
+import Entity.Usuario;
+import dao.UsuarioFacade;
+import java.io.IOException;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Javier
+ */
+@WebServlet(name = "ServletIniciarSesion", urlPatterns = {"/ServletIniciarSesion"})
+public class ServletIniciarSesion extends HttpServlet {
+
+    @EJB
+    private UsuarioFacade usuarioFacade;
+
+    protected void processRequest(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        String correo = new String(req.getParameter("correo").getBytes("ISO-8859-1"), "UTF-8");
+        String password = new String(req.getParameter("password").getBytes("ISO-8859-1"), "UTF-8");
+        
+        String mensaje = "";
+        Usuario user = usuarioFacade.find(correo);
+        
+        if (user == null) {
+            mensaje = "Ese correo no existe";
+        } else if (!user.getPassword().equals(password)) {
+            mensaje = "La contraseña no es correcta";
+        } else {
+            mensaje = "ok";
+        }
+        
+        HttpSession session = req.getSession();
+        session.setAttribute("mensajeInicioSesion", mensaje);
+        
+        if (mensaje.equals("ok")) {
+            RequestDispatcher rd;
+            rd = req.getRequestDispatcher("PaginaPrincipal.jsp");
+            rd.forward(req, res);
+        } else {
+            RequestDispatcher rd;
+            rd = req.getRequestDispatcher("IniciarSesion.jsp");
+            rd.forward(req, res);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/PaginaPrincipal.jsp
@@ -0,0 +1,24 @@
+<%-- 
+    Document   : PaginaPrincipal
+    Created on : 24-mar-2021, 21:14:22
+    Author     : Javier
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <title> EvenbriteFeka </title>
+        <link rel="icon" type="image/png" href="https://cdn.evbstatic.com/s3-build/perm_001/8a4e23/django/images/favicons/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="https://cdn.evbstatic.com/s3-build/perm_001/477279/django/images/favicons/favicon-194x194.png" sizes="194x194">
+    </head>
+    <body>
+        <%@include  file="./HtmlParts/navbar.html" %>
+        <h1>Pagina principal</h1>
+        
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    </body>
+</html>
